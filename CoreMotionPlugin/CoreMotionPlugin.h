a8710d1d4a201e243d3b29c258d702033d27c5a1
Fatih <fatakhillah.khaqo@gmail.com>
2024-10-29 15:12:48 +0700

    Initial Commit

---
+++ CoreMotionPlugin/CoreMotionPlugin.h
@@ -0,0 +1,18 @@
+//
+//  CoreMotionPlugin.h
+//  CoreMotionPlugin
+//
+//  Created by Fatakhillah Khaqo on 29/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreMotionPlugin.
+FOUNDATION_EXPORT double CoreMotionPluginVersionNumber;
+
+//! Project version string for CoreMotionPlugin.
+FOUNDATION_EXPORT const unsigned char CoreMotionPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreMotionPlugin/PublicHeader.h>
+
+
